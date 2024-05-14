3b20fd79bd306c4d8b3fee405ebdcab65b0fc52e
analaurafr <laurafernandes@outlook.com>
2024-05-14 19:40:42 -0300

    req 4

--- src/main/java/com/betrybe/museumfinder/service/MuseumServiceInterface.java
+++ src/main/java/com/betrybe/museumfinder/service/MuseumServiceInterface.java
@@ -1,8 +1,8 @@
 package com.betrybe.museumfinder.service;
 
-import com.betrybe.museumfinder.exception.InvalidCoordinateException;
 import com.betrybe.museumfinder.model.Coordinate;
 import com.betrybe.museumfinder.model.Museum;
+import java.util.Optional;
 
 /**
  * Interface for Museum service class.
