67e421205fdb64478a69d3033f0c0d5bc6b923ca
analaurafr <laurafernandes@outlook.com>
2024-05-14 20:46:57 -0300

    req 6

--- src/main/java/com/betrybe/museumfinder/controller/MuseumController.java
+++ src/main/java/com/betrybe/museumfinder/controller/MuseumController.java
@@ -2,20 +2,29 @@ package com.betrybe.museumfinder.controller;
 
 import com.betrybe.museumfinder.dto.MuseumCreationDto;
 import com.betrybe.museumfinder.dto.MuseumDto;
+import com.betrybe.museumfinder.exception.InvalidCoordinateException;
+import com.betrybe.museumfinder.exception.MuseumNotFoundException;
+import com.betrybe.museumfinder.model.Coordinate;
 import com.betrybe.museumfinder.model.Museum;
+import com.betrybe.museumfinder.service.MuseumService;
 import com.betrybe.museumfinder.service.MuseumServiceInterface;
+import com.betrybe.museumfinder.util.CoordinateUtil;
 import com.betrybe.museumfinder.util.ModelDtoConverter;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
 
 /**
  * The type Museum controller.
  */
-@Controller
+@RestController
+@RequestMapping("/museums")
 public class MuseumController {
   private MuseumServiceInterface museumService;
 
@@ -30,12 +39,35 @@ public class MuseumController {
    * @param museumCreationDto the museum creation dto
    * @return the response entity
    */
-  @PostMapping("/museums")
+  @PostMapping
   public ResponseEntity<MuseumDto> newMuseum(@RequestBody MuseumCreationDto museumCreationDto) {
     Museum newMuseum = ModelDtoConverter.dtoToModel(museumCreationDto);
     Museum savedMuseum = museumService.createMuseum(newMuseum);
 
     return ResponseEntity.status(HttpStatus.CREATED)
         .body(ModelDtoConverter.modelToDto(savedMuseum));
+  }
+
+  /**
+   * Gets closest museums.
+   *
+   * @param latitude  the latitude
+   * @param longitude the longitude
+   * @param maxDistKm the max dist km
+   * @return the closest museums
+   */
+  @GetMapping("/closest")
+  public ResponseEntity<Object> getClosestMuseums(
+      @RequestParam("lat") double latitude,
+      @RequestParam("lng") double longitude,
+      @RequestParam(value = "max_dist_km") Double maxDistKm
+  ) {
+    Coordinate coordinate = new Coordinate(latitude, longitude);
+
+    Museum closestMuseum = museumService.getClosestMuseum(coordinate, maxDistKm);
+
+    MuseumDto museumDto = ModelDtoConverter.modelToDto(closestMuseum);
+
+    return ResponseEntity.ok(museumDto);
   }
 }
